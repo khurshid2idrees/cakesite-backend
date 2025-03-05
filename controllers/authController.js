ea65b759e4e2ec92bdee82e01a8d9cfd73eb240b
khurshid idrees <umarkhurshid3@gmail.com>
2025-03-05 23:46:16 +0530

    fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,6 +52,12 @@ exports.loginUser = async (req, res) => {
       { expiresIn: "1d" }
     );
 
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+    });
+
     res.status(200).json({
       status: "success",
       message: "Login successful",
