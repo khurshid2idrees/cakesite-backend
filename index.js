88e88abb3aa0055c3136bafab29817fcb8646ed3
khurshid idrees <md.khurshid@qurilo.com>
2025-04-17 18:26:26 +0530

    Update server port configuration and increase file size limit for uploads

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 require("dotenv").config();
 
 // Define Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.POR || 5001;
 
 // Start the Express Server
 app.listen(PORT, () => {
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -17,7 +17,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB file size limit
 });
 
 module.exports = upload;
